9f2893d7862f443571895c63c5bd0490bab313ee
Unknown <baciudenis@gmail.com>
2017-07-11 21:08:25 +0000

    Add travis and gulp

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/Barsun/cv-page-react.svg?branch=master)](https://travis-ci.org/Barsun/cv-page-react)
+
 # cv-page-react
 CV welcome page build in react 
---
+++ config/build.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+
+	destination: path.resolve(__dirname, '../dist/www/'),
+	destinationAssets: path.resolve(__dirname, '../dist/www/assets')
+
+};
